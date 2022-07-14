c329c1ce7b95f5d7aac5de139ef8326625e1d13c
William Maitre <william.maitre@gmail.com>
2022-07-14 08:44:42 -0400

    finished exercise 1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Exercise Example
+
+//for (int i = 0; i < 5; i ++)
+//{
+//   Console.WriteLine($"{i}");
+//}
+
+//write this for loop 15 times
+
+
+for (int i = 0; i < 15; i ++)
+{
+    Console.WriteLine($"{i}");
+}
